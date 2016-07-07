966657215dd658d72b1183a5b78ff2791e56fe43
swati <ssawant@snappydata.io>
2016-07-07 15:05:17 +0530

    - Modified the script to read user specified value for -DremovePassedTest parameter. This is required for longRunningTest.

--- tests/core/src/main/java/bin/sample-runbt.sh
+++ tests/core/src/main/java/bin/sample-runbt.sh
@@ -8,10 +8,11 @@ usage(){
   echo " Optionally any of:" 1>&2
   echo " -l <local-conf-file-path> --   path to local conf file " 1>&2
   echo " -r <n>  -- run test suite n number of times, the default is 1" 1>&2
+  echo " -d <boolean>  -- whether to delete passed test run logs, the default value is true" 1>&2
   echo " -m mail_address -- email address to send results of run to" 1>&2
   echo " (e.g. sh sample-runbt.sh /home/snappy/gemxdRegression /home/snappy/project/snappydata sql/sql.bt sql/sqlDisk/sqlDisk.bt )" 1>&2
   echo " or"
-  echo " (e.g. sh sample-runbt.sh /home/snappy/gemxdRegression /home/snappy/project/snappydata -l /home/snappy/gemxdRegression/local.conf -r 3 sql/sql.bt sql/sqlDisk/sqlDisk.bt )" 1>&2
+  echo " (e.g. sh sample-runbt.sh /home/snappy/gemxdRegression /home/snappy/project/snappydata -l /home/snappy/gemxdRegression/local.conf -r 3  -d false sql/sql.bt sql/sqlDisk/sqlDisk.bt )" 1>&2
   exit 1
 }
 resultDir=
@@ -36,11 +37,12 @@ localconfpath=$JTESTS/sql/snappy.local.conf
 bts=
 TEST_JVM=
 mailAddrs=
+deleteTestLogs=true
 processArgs() {
   local scnt=0
   local OPTIND=1
 
-  while getopts ":l:r:" op
+  while getopts ":l:r:d:" op
   do
     case $op in
       ( "l" ) ((scnt++)) ; localconfpath=$OPTARG ;;
@@ -49,6 +51,7 @@ processArgs() {
           ( [0-9]* ) runIters=$OPTARG ;;
           ( * ) ;;
         esac ;;
+      ( "d" ) ((scnt++)) ; deleteTestLogs="$OPTARG" ;;
       ( "m" ) ((scnt++)) ; mailAddrs="${mailAddrs} $OPTARG" ;;
       ( * ) echo "Unknown argument -$OPTARG provided: see usage " ; usage ;;
     esac
@@ -94,5 +97,5 @@ export EXTRA_JTESTS=$SNAPPYDATADIR/store/tests/core/build-artifacts/linux/classe
 
 # This is the command to run the test, make sure the correct release version of jar used or change the jar path to use correctly. Also change the jar name in sql/snappy.local.conf if incorrect
 
-echo $SNAPPYDATADIR/store/tests/core/src/main/java/bin/run-snappy-store-bt.sh --osbuild $GEMFIRE $OUTPUT_DIR -Dproduct=snappystore -DtestJVM=$TEST_JVM/bin/java -DEXTRA_JTESTS=$EXTRA_JTESTS  -Dbt.grepLogs=true -DremovePassedTest=true -DnumTimesToRun=$runIters -DlocalConf=$localconfpath ${bts}
-$SNAPPYDATADIR/store/tests/core/src/main/java/bin/run-snappy-store-bt.sh --osbuild $GEMFIRE $OUTPUT_DIR -Dproduct=snappystore -DtestJVM=$TEST_JVM/bin/java -DEXTRA_JTESTS=$EXTRA_JTESTS  -Dbt.grepLogs=true -DremovePassedTest=true -DnumTimesToRun=$runIters -DlocalConf=$localconfpath ${bts}
+echo $SNAPPYDATADIR/store/tests/core/src/main/java/bin/run-snappy-store-bt.sh --osbuild $GEMFIRE $OUTPUT_DIR -Dproduct=snappystore -DtestJVM=$TEST_JVM/bin/java -DEXTRA_JTESTS=$EXTRA_JTESTS  -Dbt.grepLogs=true -DremovePassedTest=$deleteTestLogs -DnumTimesToRun=$runIters -DlocalConf=$localconfpath ${bts}
+$SNAPPYDATADIR/store/tests/core/src/main/java/bin/run-snappy-store-bt.sh --osbuild $GEMFIRE $OUTPUT_DIR -Dproduct=snappystore -DtestJVM=$TEST_JVM/bin/java -DEXTRA_JTESTS=$EXTRA_JTESTS  -Dbt.grepLogs=true -DremovePassedTest=$deleteTestLogs -DnumTimesToRun=$runIters -DlocalConf=$localconfpath ${bts}
