b3205852be0d87fb50a9c4c62c662b00920d693a
Amogh Shetkar <ashetkar@users.noreply.github.com>
2016-09-16 18:30:22 +0530

    Snap 735: Supporting VARCHAR with size and processing STRING as VARCHAR(32762), by default. (#104)
    * SNAP-735: Provide/Read size as precision for VARCHAR type.
    * Changes to render CHAR as CHAR and not VARCHAR.

--- gemfirexd/core/src/main/java/com/pivotal/gemfirexd/internal/engine/distributed/SnappyResultHolder.java
+++ gemfirexd/core/src/main/java/com/pivotal/gemfirexd/internal/engine/distributed/SnappyResultHolder.java
@@ -113,6 +113,10 @@ public final class SnappyResultHolder extends GfxdDataSerializable {
           // read the precision and the scale
           precisions[i] = (int)InternalDataSerializer.readSignedVL(dis);
           scales[i] = (int)InternalDataSerializer.readSignedVL(dis);
+        } else if (columnType == StoredFormatIds.SQL_VARCHAR_ID ||
+            columnType == StoredFormatIds.SQL_CHAR_ID) {
+          precisions[i] = (int)InternalDataSerializer.readSignedVL(dis);
+          scales[i] = -1;
         } else {
           precisions[i] = -1;
           scales[i] = -1;
@@ -243,10 +247,21 @@ public final class SnappyResultHolder extends GfxdDataSerializable {
         break;
 
       case StoredFormatIds.SQL_CLOB_ID:
-      case StoredFormatIds.SQL_VARCHAR_ID:
         dvd = new SQLClob();
         jdbcTypeId = Types.CLOB;
         dtd = DataTypeDescriptor.getBuiltInDataTypeDescriptor(jdbcTypeId, nullable);
+        break;
+
+      case StoredFormatIds.SQL_VARCHAR_ID:
+        dvd = new SQLVarchar();
+        jdbcTypeId = Types.VARCHAR;
+        dtd = DataTypeDescriptor.getBuiltInDataTypeDescriptor(jdbcTypeId, nullable, precision);
+        break;
+
+      case StoredFormatIds.SQL_CHAR_ID:
+        dvd = new SQLChar();
+        jdbcTypeId = Types.CHAR;
+        dtd = DataTypeDescriptor.getBuiltInDataTypeDescriptor(jdbcTypeId, nullable, precision);
         break;
 
       case StoredFormatIds.SQL_BLOB_ID:
@@ -254,6 +269,13 @@ public final class SnappyResultHolder extends GfxdDataSerializable {
         jdbcTypeId = Types.BLOB;
         dtd = DataTypeDescriptor.getBuiltInDataTypeDescriptor(
             jdbcTypeId, nullable);
+        break;
+
+      // indicator for complex type as clob
+      case StoredFormatIds.REF_TYPE_ID:
+        dvd = new SQLClob();
+        jdbcTypeId = Types.CLOB;
+        dtd = DataTypeDescriptor.getBuiltInDataTypeDescriptor(jdbcTypeId, nullable);
         break;
 
       default :
