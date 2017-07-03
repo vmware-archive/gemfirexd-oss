58f203ada62d7a3cff0956a6311927a0ce7ae4e8
kneeraj <kneeraj@snappydata.io>
2017-07-03 16:36:02 +0530

    Avoid qinfo creation for internal queries of smart connector executors. (#219)
    * Avoid qinfo creation for internal queries of smart connector executors.
    Fixes SNAP-1699.

--- gemfirexd/core/src/main/java/com/pivotal/gemfirexd/internal/impl/sql/GenericStatement.java
+++ gemfirexd/core/src/main/java/com/pivotal/gemfirexd/internal/impl/sql/GenericStatement.java
@@ -653,7 +653,8 @@ public class GenericStatement
 				  StringBuilder queryTextForStats = null;
 				  boolean continueLoop = true;
 			    int i = 0;
-			    boolean forceSkipQueryInfoCreation = false;
+			    boolean forceSkipQueryInfoCreation = Misc.getMemStore().isSnappyStore()
+					  && lcc.getBucketIdsForLocalExecution() != null;
 			    while (continueLoop) {
 			      i++;
 			      continueLoop = false;
