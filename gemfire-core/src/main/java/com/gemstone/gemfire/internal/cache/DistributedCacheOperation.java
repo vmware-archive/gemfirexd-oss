77a10b5f8f5ee9f6d26c7893a7db7ec50d7ce89a
Sumedh Wale <swale@snappydata.io>
2018-01-14 02:33:05 +0530

    Fixed another hang due to NPE in DistributedCacheOperation

--- gemfire-core/src/main/java/com/gemstone/gemfire/internal/cache/DistributedCacheOperation.java
+++ gemfire-core/src/main/java/com/gemstone/gemfire/internal/cache/DistributedCacheOperation.java
@@ -1304,8 +1304,10 @@ public abstract class DistributedCacheOperation {
         SystemFailure.checkFailure();
         thr = t;
       } finally {
-        checkVersionIsRecorded(this.versionTag, lclRgn,
-            event.getOperation().isEntry() ? (EntryEventImpl)event : null);
+        if (event != null) {
+          checkVersionIsRecorded(this.versionTag, lclRgn,
+              event.getOperation().isEntry() ? (EntryEventImpl)event : null);
+        }
         if (sendReply) {
           // logger.fine("basicProcess: <" + this + ">: sending reply");
           ReplyException rex = null;
