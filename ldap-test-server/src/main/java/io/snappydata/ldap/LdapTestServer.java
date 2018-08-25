d98bde9d8562f9c3f84377315c8e3ec2b0fbeacd
Sumedh Wale <swale@snappydata.io>
2018-08-25 13:24:19 +0530

    Cleaning up ldap-test-server
    * removed Apache Directory Server jars from repository
    * build a "shadow" jar instead dynamically the first time in "dist" directory

--- ldap-test-server/src/main/java/io/snappydata/ldap/LdapTestServer.java
+++ ldap-test-server/src/main/java/io/snappydata/ldap/LdapTestServer.java
@@ -15,6 +15,24 @@
  * LICENSE file.
  */
 /*
+ * Changes for SnappyData distributed computational and data platform.
+ *
+ * Portions Copyright (c) 2018 SnappyData, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
+ */
+/*
  * Adapted from org.apache.directory.seserver.EmbeddedADSVer157 example from
  * Apache Directory Server distribution having the license below.
  *
--- ldap-test-server/start-ldap-server.sh
+++ ldap-test-server/start-ldap-server.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-java -cp './jars/*' io.snappydata.ldap.LdapTestServer "$@"
+function absPath() {
+  perl -MCwd -le 'print Cwd::abs_path(shift)' "$1"
+}
+
+cwd="$(dirname "$(absPath "$0")")"
+
+if [ ! -f "${cwd}/dist/ldap-test-server-1.0.0.jar" ]; then
+  "${cwd}/gradlew" build
+fi
+
+java -cp "${cwd}"'/dist/*' io.snappydata.ldap.LdapTestServer "$@"
